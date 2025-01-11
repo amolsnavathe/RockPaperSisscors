cae391efd55da05d2525aa0733140e7bf38661c6
Amol Navathe <asnavath@ncsu.edu>
2025-01-11 17:48:21 -0500

    RockPaperScissors - 1

---
+++ rockpaperscissors.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+
+public class RockPaperSissors {
+    public static void main(String[] args) {
+
+        String [] rps = 'r', 'p', 's';
+        String CompMove = rps[new Random(). nextInt(rps.length)];
+        String PlayerMove;
+
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please select your move(r for rock, p for paper, s for scissors):");
+            PlayerMove = scanner.nextLine();
+            if (PlayerMove.equals('r') || PlayerMove.equals('p') || PlayerMove.equals('s')) {
+                break;
+            }
+            System.out.println(PlayerMove + " is not a valid move");
+        }
+        System.out.println(CompMove + PlayerMove);
+
+        if (PlayerMove.equals(CompMove)) {
+            System.out.println("It's a Tie!");
+        }
+        else if (PlayerMove.equals("r")) {
+            if (CompMove.equals("p")){
+                System.out.println("You Lose!");
+            } else if (CompMove.equals("s")) {
+                System.out.println("You Win!");
+            }
+        }
+        else if (PlayerMove.equals("p")) {
+            if (CompMove.equals("r")){
+                System.out.println("You Win!");
+            } else if (CompMove.equals("s")) {
+                System.out.println("You Lose!");
+            }
+        }
+        else if (PlayerMove.equals("s")) {
+            if (CompMove.equals("p")){
+                System.out.println("You Lose!");
+            } else if (CompMove.equals("r")) {
+                System.out.println("You Win!");
+            }
+        }
+    }
+};
